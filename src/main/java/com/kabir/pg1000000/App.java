f3f8d70b4212dd23179ad73e760801a988b2f611
Hasi <hasi22ainds@cmrit.ac.in>
2025-06-09 09:18:44 +0530

    first

---
+++ src/main/java/com/kabir/pg1000000/App.java
@@ -0,0 +1,13 @@
+package com.kabir.pg1000000;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
